c1930d71965364dc1ded4e77b72ce3c84d045fb3
niranjan-web <niranjanrk56@gmail.com>
2022-08-15 20:59:29 +0530

    add a file named FileWriterDemo

--- hello world/src/FileWriterDemo.java
+++ hello world/src/FileWriterDemo.java
@@ -3,10 +3,18 @@ import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class FileWriterDemo {
 
 	public static void main(String[] args) throws IOException {
+		
+		InputStreamReader input = new InputStreamReader(System.in);
+		BufferedReader buffer = new BufferedReader(input);
+		System.out.println("enter your fucking name");
+		String name = buffer.readLine();
+		
+		System.out.println(name);
 		BufferedWriter writer = new BufferedWriter(new FileWriter("input.txt"));
 		String arr[] = {"ragu","rahul","anothony","aravind","akon"};
 		
